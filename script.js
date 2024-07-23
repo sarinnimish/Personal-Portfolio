622a1e2044c8ed86ef2a7fa1a3889e8375e191db
sarinnimish <105263948+sarinnimish@users.noreply.github.com>
2024-07-23 23:30:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+  
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+  }
+  showMenu('nav-toggle','nav-menu')
+  
+  /*==================== REMOVE MENU MOBILE ====================*/
+  const navLink = document.querySelectorAll('.nav__link')
+  
+  function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+  }
+  navLink.forEach(n => n.addEventListener('click', linkAction))
+  
+  /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+  const sections = document.querySelectorAll('section[id]')
+  
+  const scrollActive = () =>{
+    const scrollDown = window.scrollY
+  
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+  }
+  window.addEventListener('scroll', scrollActive)
+  
+  /*===== SCROLL REVEAL ANIMATION =====*/
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+  //     reset: true
+  });
+  
+  sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+  sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+  sr.reveal('.home__social-icon',{ interval: 200}); 
+  sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+  
+//   =============Contact form =============
+  const form = document.getElementById("form");
+  const result = document.getElementById("result");
+  
+  form.addEventListener("submit", function (e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+    var object = {};
+    formData.forEach((value, key) => {
+      object[key] = value;
+    });
+    var json = JSON.stringify(object);
+    result.innerHTML = "Please wait...";
+  
+    fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    })
+      .then(async (response) => {
+        let json = await response.json();
+        if (response.status == 200) {
+          result.innerHTML = json.message;
+          result.classList.remove("text-gray-500");
+          result.classList.add("text-green-500");
+        } else {
+          console.log(response);
+          result.innerHTML = json.message;
+          result.classList.remove("text-gray-500");
+          result.classList.add("text-red-500");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        result.innerHTML = "Something went wrong!";
+      })
+      .then(function () {
+        form.reset();
+        setTimeout(() => {
+          result.style.display = "none";
+        }, 5000);
+      });
+  });
